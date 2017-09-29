685273d34707837c5488af2b7268253d983da60b
Clément Lusson <clementlusson@gmail.com>
2017-09-29 14:29:17 +0200

    set rabbit port on url

--- rabbit/connectionService.js
+++ rabbit/connectionService.js
@@ -2,7 +2,8 @@ var amqp = require('amqplib/callback_api')
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    const URL = 'amqp://' + process.env.RABBIT_USER + ':' + process.env.RABBIT_PASSWORD + '@' + process.env.RABBIT_HOST
+    const URL = 'amqp://' + process.env.RABBIT_USER + ':' +
+      process.env.RABBIT_PASSWORD + '@' + process.env.RABBIT_HOST + ':' + process.env.RABBIT_PORT
     amqp.connect(URL, function (err, conn) {
       if (err) {
         reject(new Error('Connection refusée'))
