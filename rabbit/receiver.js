d09771d5a8a18fd472b1034eb60239f6b83c90a3
COULONNIER <nicolas.coulonnier@sogeti.com>
2017-10-09 13:23:54 +0200

    allez la ca marche

--- rabbit/receiver.js
+++ rabbit/receiver.js
@@ -16,8 +16,7 @@ module.exports = (conn) => {
       ch.bindQueue(queue_name, ex, severity)
 
       ch.consume(queue_name, function (msg) {
-        console.log(typeof msg)
-        let message = JSON.parse(msg)
+        let message = Json.parse(msg.content.toString())
         console.log(message.userid+" "+ message.content)
         sendTextMessage(message.userid, message.content)
       }, { noAck: true })
