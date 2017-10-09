053f35f83e6687b03c64ed4962b2cc8a6ee5d997
clusson <clementlusson@gmail.com>
2017-10-09 10:07:43 +0200

    replace export function

--- bot.js
+++ bot.js
@@ -125,31 +125,30 @@ function sendFirst(event, user) {
 //////////////////////////
 
 
-module.exports = {
-  sendTextMessage: sendTextMessage
+module.exports = () => {
+  function sendTextMessage(recipientId, messageText) {
+    
+      let messageData = { text: messageText }
+    
+      request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: process.env.VERIFY_TOKEN,
+        method: 'POST',
+        json: {
+          recipient: { id: recipientId },
+          message: messageData,
+        }
+      }, function (error, response, body) {
+        if (error) {
+          debug.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+          debug.log('Error: ', response.body.error)
+        }
+      })
+      // TODO - Get data from receiver
+    }
 }
 
-function sendTextMessage(recipientId, messageText) {
-
-  let messageData = { text: messageText }
-
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: process.env.VERIFY_TOKEN,
-    method: 'POST',
-    json: {
-      recipient: { id: recipientId },
-      message: messageData,
-    }
-  }, function (error, response, body) {
-    if (error) {
-      console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
-    }
-  })
-  // TODO - Get data from receiver
-}
 
 
 
