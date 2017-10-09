519f6cec9bf166f74493a743c2964164a244a76b
clusson <clementlusson@gmail.com>
2017-10-09 17:09:45 +0200

    add severity

--- rabbit/publisher.js
+++ rabbit/publisher.js
@@ -9,8 +9,9 @@ module.exports = (conn, message, user) => {
     const type = process.env.RABBIT_TYPE
     const ex = process.env.RABBIT_EXCHANGE
     const queue_name = process.env.RABBIT_QUEUE_API_MESSAGE
-    const queue_user = process.env.RABBIT_QUEUE_API_USER
     const severity = process.env.RABBIT_BINDING_API_MESSAGE
+    const queue_user = process.env.RABBIT_QUEUE_API_USER
+    const severityUser = process.env.RABBIT_BINDING_API_USER
 
     const messageStr = JSON.stringify(message)
     const messagewUserStr = JSON.stringify(message, user)
@@ -18,7 +19,7 @@ module.exports = (conn, message, user) => {
     ch.assertExchange(ex, type, { durable: true })
     
     if (user){
-      ch.bindQueue(queue_user, ex, severity)
+      ch.bindQueue(queue_user, ex, severityUser)
       ch.publish(ex, severity, new Buffer(messagewUserStr))
     } else {
       ch.bindQueue(queue_name, ex, severity)
